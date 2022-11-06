79d59d8cc6a1970e9c2725396012f71bff48acfa
Nazar Koval <n.kovaldev@gmail.com>
2022-11-06 17:41:26 +0200

    started: 11 - Custom Video Player

---
+++ 11 - Custom Video Player/script.js
@@ -0,0 +1,37 @@
+/* Declaration */
+const player = document.querySelector(".player")
+
+const video = player.querySelector(".viewer")
+const progress = player.querySelector(".progress")
+const progressBar = player.querySelector(".progress__filled")
+const toggle = player.querySelector(".toggle")
+const skipButtons = player.querySelectorAll("[data-skip]")
+const ranges = player.querySelectorAll(".player__slider")
+
+/* Functions */
+function togglePlay() {
+   const method = video.paused ? "play" : "pause"
+   video[method]()
+}
+
+function updateButton() {
+   const icon = this.paused ? "►" : "❚ ❚"
+   toggle.textContent = icon
+}
+
+function skip() {
+   video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+   video[this.name] = this.value
+}
+
+/* Hook up */
+video.addEventListener("click", togglePlay)
+video.addEventListener("play", updateButton)
+video.addEventListener("pause", updateButton)
+toggle.addEventListener("click", togglePlay)
+skipButtons.forEach(button => button.addEventListener("click", skip))
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate))
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate))
